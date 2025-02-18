a005fd4a3010bb5a5321a4a988766e469ab2a199
Denar Padilla Gutierrez <83237161+Dencito@users.noreply.github.com>
2025-02-17 21:12:43 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 const authRoutes = require('./routes/auth');
 const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/orders');
-const { db } = require('./utils/db');
+//const { db } = require('./utils/db');
 
 
 const app = express();
@@ -16,21 +16,24 @@ app.get("/pruebas", (req, res) => {
   res.json({ message: "Hola esto es una prueba" });
 });
 
-
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+/*
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 
+
+
 db.then(() => {
   console.log('Connected to MongoDB');
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
+  
 })
 .catch((error) => {
   console.error('Failed to connect to MongoDB', error);
-})
+}) */
 
 
 
